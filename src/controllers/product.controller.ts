54f5bbfe7c08c86168ade643de5d301ef57b92b3
Deepak <deepak2603dk@gmail.com>
2024-05-24 05:43:57 +0530

    product get and update contollers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 import Product, { ProdDocument } from '../model/product.model';
 
+
+
+
+//create product
+//admin route
 export const createProduct = async (req: Request, resp: Response): Promise<void> => {
   try {
     const productDetail: ProdDocument = req.body;
@@ -23,3 +28,44 @@ export const createProduct = async (req: Request, resp: Response): Promise<void>
     resp.status(500).send({ error: error.message });
   }
 };
+
+
+
+
+//get all product
+export const getAllProducts = async (req: Request, resp: Response): Promise<void> => {
+  try {
+    const products = await Product.find();
+    
+    if (products.length === 0) {
+      resp.status(404).send({ message: 'No products found' });
+      return;
+    }
+    
+    resp.status(200).send({ products });
+  } catch (error: any) {
+    resp.status(500).send({ error: error.message });
+  }
+};
+
+
+
+
+// Update product function
+export const updateProduct = async (req: Request, resp: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const productDetail = req.body;
+  
+      const product = await Product.findByIdAndUpdate(id, productDetail, { new: true, runValidators: true });
+  
+      if (!product) {
+        resp.status(404).send({ message: 'Product not found' });
+        return;
+      }
+  
+      resp.status(200).send({ product });
+    } catch (error: any) {
+      resp.status(500).send({ error: error.message });
+    }
+  };
--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,9 +1,11 @@
 import express,{request,response} from "express";
-import { createProduct } from "../controllers/product.controller";
+import { createProduct, getAllProducts ,updateProduct} from "../controllers/product.controller";
 const router = express.Router();
 
 
 router.route('/create').post(createProduct)
+router.route('/allProducts').get(getAllProducts)
+router.route('/update/:id').put(updateProduct)
 
 
 
