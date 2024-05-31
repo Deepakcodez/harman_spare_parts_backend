ec11b64ee47a2022edb2c4dd1c24cbc2ee7553be
Deepak <deepak2603dk@gmail.com>
2024-05-31 20:48:04 +0530

    connect atlas try1

--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
-const uri: string = 'mongodb://127.0.0.1:27017/harman-spare-parts'; // for localhost mongo
+// const uri: string = 'mongodb://127.0.0.1:27017/harman-spare-parts'; // for localhost mongo
 
+
+const uri =  process.env.MONGO_URI
 async function connectDB(): Promise<void> {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri!);
     console.log('>>>>>>>>>>> db connected');
   } catch (error:any) {
     console.error('Error connecting to the database:', error.message);
