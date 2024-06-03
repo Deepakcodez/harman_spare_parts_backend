ed274d96943b8f9c3cf56ca62ca66584ed4a8716
Deepak <deepak2603dk@gmail.com>
2024-06-04 01:31:23 +0530

    increase number of product on one page

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -28,7 +28,7 @@ export const createProduct = asyncHandler(
 // Get all products
 export const getAllProducts = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const resultPerPage: number = 2;
+    const resultPerPage: number = 20;
 
     const productCount = await Product.countDocuments();
 
